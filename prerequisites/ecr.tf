14c9a95645e9d642ada6fc1eeb96c9558244e575
nbcchen <hanjun.chen@nbcuni.com>
2024-07-17 12:18:30 -0400

    no need to create kafka repo

--- prerequisites/ecr.tf
+++ prerequisites/ecr.tf
@@ -1,7 +1,3 @@
-resource "aws_ecr_repository" "kafka_broker_repository" {
-  name = "kafka-broker-ecr-repo-${var.env}"
-}
-
 resource "aws_ecr_repository" "kafka_producer_repository" {
   name = "kafka-producer-ecr-repo-${var.env}"
 }
