23f673cf5fdba600c2255a0e6e07f9eeda3136e5
ShrutikaSingh <shrutika051220@gmail.com>
2024-07-18 16:28:58 -0700

    update tags

--- prerequisites/main.tf
+++ prerequisites/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
   default_tags {
     tags = {
       terraform         = "yes"
-      environment       = var.env
-      costbucketproject = "kafka-poc"
-      costbucket        = "koco"
       application       = "oneapp"
+      environment       = var.env
+      costbucket        = "devops"
+      costbucketproject = "kafka"
     }
   }
 }
